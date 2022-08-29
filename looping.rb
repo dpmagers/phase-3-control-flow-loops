1b16e666824108ae96629afa5d9c5522dd82575a
Daniel Magers <daniel.magers@gmail.com>
2022-08-29 07:01:32 -0500

    commit tree d0115812326097d1bab240b1b9a0b1f9aa0652a9

--- looping.rb
+++ looping.rb
@@ -1,6 +1,21 @@
 def happy_new_year
-  # your code here
-end
+  counter = 10
+  until counter == 0
+    puts "#{counter}"
+    counter -= 1
+  end
+end 
+
+
+# function happyNewYear() {
+#   let counter = 10;
+#   while (counter > 0) {
+#     console.log(counter);
+#     counter--;
+#   }
+#   console.log("Happy New Year!");
+# }
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -22,3 +37,37 @@ end
 def reverse_string(str)
   # your code here
 end
+
+
+
+# i = 0
+# while i < 5
+#   puts "Looping!"
+#   i += 1
+# end
+
+#WHILE LOOP
+# i = 0 
+# while i < 5
+#   puts "Looping!"
+#   i += 1
+# end
+
+#UNTIL 
+# counter = 0
+# until counter == 10
+#   puts "Counting!"
+#   counter += 1
+# end
+
+# #TIMES (works like for loop in js)
+# 10.times do |i|
+#   puts "Looping!"
+#   puts "i is: #{i}"
+# end
+
+# 10.times { |i| puts "i is: #{i}" }
+
+# (16..20).each do |num|
+#   puts num
+# end
